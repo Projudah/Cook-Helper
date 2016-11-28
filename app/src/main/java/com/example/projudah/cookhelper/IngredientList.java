0a4ab58416537165ef6fd56762cdb65276487718
wm17 <wes.martin17@gmail.com>
2016-11-28 00:18:25 -0500

    recipe object and ingredient list

---
+++ app/src/main/java/com/example/projudah/cookhelper/IngredientList.java
@@ -0,0 +1,39 @@
+
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * Created by Wes on 2016-11-25.
+ */
+public class IngredientList<String> extends ArrayList<String> {
+  
+  File file;
+  public IngredientList(){
+    file = new File("ingredients.json");
+    if(!file.exists()){
+      try{
+        file.createNewFile();
+      }catch(IOException e){}
+    }
+    else this = readRecipe();
+  }
+  
+   public void writeRecipe() throws JsonGenerationException, JsonMappingException, IOException{
+     ObjectMapper mapper = new ObjectMapper();
+     mapper.writeValue(file,this);
+   }
+   
+   public IngredientList<String> readRecipe() throws JsonParseException, JsonMappingException, IOException{
+     ObjectMapper mapper = new ObjectMapper();
+     IngredientList<String> fuckthis = new IngredientList<String>();
+     fuckthis = mapper.readValue(file, IngredientList.class);
+     return fuckthis;
+   }
+   
+}
---
+++ app/src/main/java/com/example/projudah/cookhelper/Recipe.java
@@ -0,0 +1,88 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.io.File;
+import java.io.IOException;
+
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.SerializationConfig;
+
+
+/**
+ * Created by Wes on 2016-11-24.
+ */
+public class Recipe {
+
+    public String name;
+    public String category;
+    public ArrayList<String> ingredients;
+
+    public Recipe(){
+        this.name="";
+        this.category="";
+        ingredients = new ArrayList<String>();
+        this.ingredients.add("Test ingredient");
+    }
+
+    public String getName(){
+        return this.name;
+    }
+    
+    public String toString(){
+        return("Name:"+this.getName()+" Category:"+this.category+ "Ingredients: "+ingredients.get(0));
+    }
+
+    public void writeRecipe() throws JsonGenerationException, JsonMappingException, IOException{
+        ObjectMapper mapper = new ObjectMapper();
+        String recipeName = this.getName();
+        mapper.writeValue(new File(recipeName+".json"), this);
+    }
+
+    private Recipe readRecipe(File file) throws JsonParseException, JsonMappingException, IOException{
+        ObjectMapper mapper = new ObjectMapper();
+        Recipe returnRecipe = new Recipe();
+        returnRecipe = mapper.readValue(file, Recipe.class);
+        return returnRecipe;
+    }
+    
+    public void addIngredient(String ingredient){
+        this.ingredients.add(ingredient);
+    }
+    
+    public static void main (String[] args){
+        ObjectMapper mapper = new ObjectMapper();
+        Recipe test = new Recipe();
+        test.name = "this is test";
+        test.category = "category";
+        test.addIngredient("butter");
+        try {
+            test.writeRecipe();
+        }
+        catch(IOException e){}
+        Recipe test2 = new Recipe();
+        test2.category = "category";
+        try {
+            test2 = test2.readRecipe(new File("this is test.json"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(test2.toString());
+        
+        IngredientList<String> testlist = new IngredientList<String>();
+        testlist.add("ham");
+        try{
+          testlist.writeRecipe();
+        }catch(Exception e){}
+        IngredientList<String> testlist2 = new IngredientList<String>();
+        try{
+          testlist2 = testlist2.readRecipe();
+        }catch(Exception e){}
+        testlist2.add("Turkey");
+        try{
+          testlist2.writeRecipe();
+        }catch(Exception e){}
+    }
+
+}
