f0af32ed2defe2d8b3d8409d415dc10bcfaadd25
hanif-ariffin <hanif.ariffin.4326@gmail.com>
2016-11-29 13:21:26 -0500

    updates to latest version

---
+++ app/src/main/java/com/example/projudah/cookhelper/Search.java
@@ -0,0 +1,17 @@
+package com.example.projudah.cookhelper;
+
+import java.util.HashMap;
+
+/**
+ * Created by r3xas on 11/29/2016.
+ */
+
+public class Search<K, V> {
+
+    HashMap<K, V> map;
+    public Search(HashMap<K, V> map){
+        this.map = map;
+    }
+
+    public
+}
